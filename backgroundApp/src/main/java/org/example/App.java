8d4067ce53e2d78dfac52166bd1d1bec30f4e1e0
Alexander Wiklund <alexander.wiklund1@gmail.com>
2020-10-15 13:53:32 +0200

    Skapat en modul för varje program och en gemensam modul Common

---
+++ backgroundApp/src/main/java/org/example/App.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Account acc = new Account("123", "Hej", "Hej");
+        System.out.println(acc.getAccountNumber());
+    }
+}
--- common/src/main/java/org/example/App.java
+++ common/src/main/java/org/example/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Account acc = new Account("Hej", "Hej", "Hej");
+    }
+
+    public static void saveToDataFile() {
+
     }
 }
