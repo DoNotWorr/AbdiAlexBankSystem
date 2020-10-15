948103009c4d1fd18789c4b1e7efc86477516181
Abdi_ <abdi_665@hotmail.com>
2020-10-15 12:54:38 +0200

    Skapade klart Accountklassen nu!

--- src/main/java/org/example/Account.java
+++ src/main/java/org/example/Account.java
@@ -1,4 +1,25 @@
 package org.example;
 
 public class Account {
+
+    private String accountNumber;
+    private String ownerID;
+    private String amount;
+
+    public Account(String accountNumber, String ownerID, String amount ){
+        this.accountNumber = accountNumber;
+        this.ownerID = ownerID;
+        this.amount = amount;
+    }
+
+
+public String getAccountNumber(){
+    return accountNumber;
+}
+public String getOwnerID(){
+        return ownerID;
+}
+public String getAmount(){
+        return amount;
+}
 }
