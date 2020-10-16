da7faf2268803bb4c29b5d1ff6d2306683bc18ee
Alexander Wiklund <alexander.wiklund1@gmail.com>
2020-10-16 14:06:23 +0200

    Ändrade Account konstruktor till att ta emot ett Customer-objekt och hämta ut ownerID

--- common/src/main/java/org/example/Customer.java
+++ common/src/main/java/org/example/Customer.java
@@ -1,12 +1,25 @@
 package org.example;
 
+import java.util.ArrayList;
+
 /**
  * @author Alex
  */
 public class Customer {
     private String firstName;
     private String lastName;
+    private String ownerID;
+    private ArrayList<String> yourAccounts = new ArrayList<>();
 
+    public Customer(String firstName, String lastName, String ownerID) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.ownerID = ownerID;
+    }
+
+    public String getOwnerID() {
+        return this.ownerID;
+    }
     //förnamn, efternamn, ägare-ID) Tillägg? ArrayList<String kontonr> yourAccounts
     //metod listAccounts
 }
