456ee428aaa31a3ec9fd73ec8bf06130ffe12f3e
Alexander Wiklund <alexander.wiklund1@gmail.com>
2020-10-19 17:27:58 +0200

    Skapade getYourAccounts() i Customer-klassen.

--- customerApp/src/main/java/org/example/App.java
+++ customerApp/src/main/java/org/example/App.java
@@ -11,17 +11,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        Customer cust = new Customer("Alex", "Wiklund", "920317");
-        Account acc1 = new Account("123123", cust);
-        cust.addNewAccount("ännumerfel");
-        cust.getYourAccounts().forEach(konto -> {
-            System.out.println(konto);
-        });
-        cust.getYourAccounts().add("felfel");
-        cust.getYourAccounts().forEach(konto -> {
-            System.out.println(konto);
-        });
-        System.out.println();
 
     }
 }
