bd6a140d8874591fdd936449f03774283c298fe6
Abdi_ <abdi_665@hotmail.com>
2020-10-20 15:24:02 +0200

    Fixade några saker på Accountklassen. Skapande employee-meny. Skapnde ett Singletonklass för Scanner men jag ska försöka göra den klart den också.

--- common/src/main/java/org/example/Account.java
+++ common/src/main/java/org/example/Account.java
@@ -1,11 +1,17 @@
 package org.example;
-
 import java.time.LocalDate;
 import java.util.ArrayList;
 
 /**
  * @author Abdi
+ *
  */
+
+// Metod addTransfer (från konto: till konto, belopp, överföringsdatum)
+    // metod removeTransfer
+    // metod insertMoney
+    // metod withrawMoney
+    // Kolla så att personen finns
 
 public class Account {
 
@@ -13,24 +19,73 @@ public class Account {
     private String ownerID;
     private int balance;
 
+    /**
+     * @param accountNumber kundens kontonummer
+     * @param customer      kundens ownerID/personnummer
+     * @param balance       belopp i kontot
+     */
 
-    public Account(String accountNumber, Customer customer) {
+    public Account(String accountNumber, Customer customer, int balance) {
         this.accountNumber = accountNumber;
-        customer.addNewAccount(accountNumber); //Alex: Lägger nytt kontonummer i kundens lista med kontonummer
+        customer.addNewAccount(accountNumber);
         this.ownerID = customer.getOwnerID();
-        this.balance = 0; //Alex: Ändrar så man inte kan välja balance när man skapar ett konto
+        this.balance = balance;
+        this.balance = 0; // vet inte vad du menar här Alex...
     }
-
+    
+    /**
+     * @return AccountNumber/Kontonummer (String)
+     */
     public String getAccountNumber() {
         return accountNumber;
     }
 
+    /**
+     * @return OwnerID/personnummer (String)
+     */
     public String getOwnerID() {
         return ownerID;
     }
 
+    /**
+     *@return Balace/Belopp på kontot (int)
+     */
     public int getBalance() {
         return balance;
+    }
+
+    /**
+     *@author Abdi
+     * Metod för att ta ut pengar.
+     */
+    public void withdraMoney(){
+        //  System.out.println("Enter the Amount you want to withdraw: ");
+        //  int amount = Scanner.nextLine()
+       //   if ( balance >= amount){
+       //   balance = balance - amount;
+       //   else {
+       //   System.out.println("Less Balance...Transaction faild");
+    }
+
+    /**
+     * Metod för att sätta in pengar.
+     */
+    public void depositMoney(int amoun){
+        //    System.out.println("Enter the Amount you want to Deposit: );
+        //    int amount = Scanner.nextInt();
+        //     if (balance != 0){
+        //     balance = balance + amount;
+    }
+
+    // TODO Vi kanske behöver skapa en till metod som vi kan skriva ut sen
+    //  TODO hur mycket penagr som man har tagit ut eller lagt in.
+
+    /**
+     * Metod för att direktövering.
+     * ska vi skapa metoden här eller i Transfer klassen ? // vi behöver snacka om det.
+     */
+    public void directTransfer(){
+
     }
 
     /**
@@ -48,5 +103,6 @@ public class Account {
         //for loop som går igenom alla konton
         //printar ut info som vi vill printa ut för varje konto
     }
+
 
 }
