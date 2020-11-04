bf52e6714a38e2a7b9da8154930ac1ab81f8ad94
Alexander Wiklund <alexander.wiklund1@gmail.com>
2020-11-04 14:21:26 +0100

    Har gjort färdigt backgroundApps struktur. Det enda som är kvar är att justera när Transfer är ändrad.

--- backgroundApp/src/main/java/org/example/backgroundApp.java
+++ backgroundApp/src/main/java/org/example/backgroundApp.java
@@ -4,124 +4,37 @@ package org.example;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 
 /**
+ *
  * @author Alex
  */
 public class backgroundApp {
 
-    //todo allCustomers behövs inte?
-    //private static HashMap<String, Customer> allCustomers = FileService.INSTANCE.loadCustomers();
+
+    //customers.txt innehåll:  {"20000723-2382":{"firstName":"Abdi","lastName":"Hussein","ownerID":"20000723-2382"},"19920317-0577":{"firstName":"Alex","lastName":"Wiklund","ownerID":"19920317-0577"}}
+    //accounts.txt innehåll: {"5536-5044-4718":{"accountName":"Abdis konto","accountNumber":"5536-5044-4718","ownerID":"20000723-2382","balance":10000,"previousTransaction":0},"5521-1299-9573":{"accountName":"Alex konto","accountNumber":"5521-1299-9573","ownerID":"19920317-0577","balance":10000,"previousTransaction":0}}
+    //transfer.txt innehåll: [{"fromAccountNumber":"5521-1299-9573","toAccountNumber":"5536-5044-4718","amount":100,"transferDate":{"year":2020,"month":8,"day":3},"status":"PENDING"},{"fromAccountNumber":"5521-1299-9573","toAccountNumber":"5536-5044-4718","amount":100000,"transferDate":{"year":2020,"month":8,"day":4},"status":"PENDING"},{"fromAccountNumber":"5521-1299-9573","toAccountNumber":"5536-5044-4718","amount":100,"transferDate":{"year":2020,"month":8,"day":5},"status":"PENDING"},{"fromAccountNumber":"5521-1299-9573","toAccountNumber":"4536-5044-4718","amount":100,"transferDate":{"year":2020,"month":11,"day":3},"status":"PENDING"},{"fromAccountNumber":"5521-1299-9573","toAccountNumber":"5536-5044-4718","amount":100,"transferDate":{"year":2020,"month":11,"day":4},"status":"CANCELLED"},{"fromAccountNumber":"5521-1299-9573","toAccountNumber":"5536-5044-4718","amount":100,"transferDate":{"year":2020,"month":11,"day":5},"status":"PENDING"}]
+
+    //Ger följande tester:
+    //2020-08-03 -> Deleted  3 months old
+    //2020-08-04 -> False: Belopp
+    //2020-08-05 -> True på alla -> COMPLETED
+    //2020-11-03 -> False: mottagarens konton existerar
+    //2020-11-04 -> False: "pending"
+    //2020-11-05 -> False: Om transferDate idag eller tidigare
 
     private static HashMap<String, Account> allAccounts = FileService.INSTANCE.loadAccounts();
     private static ArrayList<Transfer> allTransfers = FileService.INSTANCE.loadTransfers();
 
     public static void main(String[] args) {
-        long monthsBeforeDeletingHistory = 3; //Månader innan historik raderas
+        final long monthsBeforeDeletingHistory = 3; //Månader innan gammal historik raderas
+        Collections.sort(allTransfers, Comparator.comparing(Transfer::getTransferDate)); //Samtlig kod nedanför bygger på att listan är sorterad.
 
-
-/*
-        //TEST SKAPA KUNDER v
-        Customer c1 = null;
-        Customer c2 = null;
-        try {
-            c1 = new Customer("Alex", "Wiklund", "19920317-0577");
-            c2 = new Customer("Abdi", "Hussein", "20000723-2382");
-            allCustomers.put(c1.getOwnerID(), c1);
-            allCustomers.put(c2.getOwnerID(), c2);
-        } catch (InvalidNameException e) {
-            e.printStackTrace();
-        } catch (InvalidOwnerIDException e) {
-            e.printStackTrace();
-        }
-
-        Account a1 = new Account("Alex konto", c1); //kontonummer "5579 3237 7830"
-        Account a2 = new Account("Abdis konto", c2); //kontonummer "5505 5023 3139"
-        allAccounts.put(a1.getAccountNumber(), a1);
-        allAccounts.put(a2.getAccountNumber(), a2);
-
-        System.out.println("Alex: " + a1.getAccountNumber());
-        System.out.println("Abdi: " + a2.getAccountNumber());
-
-
-        Transfer t1 = new Transfer("5579 3237 7830", "5505 5023 3139", 100, LocalDate.parse("2020-10-28"));
-        Transfer t2 = new Transfer("5579 3237 7830", "5505 5023 3139", 100, LocalDate.parse("2020-07-28"));
-        Transfer t3 = new Transfer("5579 3237 7830", "5505 5023 3139", 100, LocalDate.parse("2020-07-27"));
-        Transfer t4 = new Transfer("5579 3237 7830", "5505 5023 3139", 100, LocalDate.parse("2020-10-29"));
-        Transfer t5 = new Transfer("5579 3237 7830", "5505 5023 3139", 100, LocalDate.parse("2020-10-30"));
-        Transfer t6 = new Transfer("5579 3237 7830", "5505 5023 3139", 100, LocalDate.parse("2020-10-27"));
-        allTransfers.add(t1);
-        allTransfers.add(t2);
-        allTransfers.add(t3);
-        allTransfers.add(t4);
-        allTransfers.add(t5);
-        allTransfers.add(t6);
-        //TEST SKAPA KUNDER ^
-*/
-
-
-        tempSortAndPrint("Lista från början:"); //TEST SKRIVA UT
-
-        //Sortera lista på transferDate, tidigast datum först
-        Collections.sort(allTransfers, (a, b) -> a.getTransferDate().compareTo(b.getTransferDate()));
-
-        deleteHistory(monthsBeforeDeletingHistory);
-
-        tempSortAndPrint("Har tagit bort alla som är äldre än 3 månader:"); //TEST SKRIVA UT
-
-        for (Transfer transfer : allTransfers) {
-
-            //Om transferDate ska inträffa idag eller tidigare
-            if (transfer.getTransferDate().isBefore(LocalDate.now().plusDays(1))) {
-
-                //Programmet hanterar enbart bankuppdragt som har status "pending"
-                if (transfer.getStatus() == Transfer.TransferStatus.PENDING) {
-
-                    //Kontrollera att avsändaren och mottagarens konton existerar
-                    if (allAccounts.containsKey(transfer.getFromAccountNumber()) && allAccounts.containsKey(transfer.getToAccountNumber())) {
-
-                        //Kontrollera att det finns pengar att betala med
-                        if (transfer.getAmount() <= allAccounts.get(transfer.getFromAccountNumber()).getBalance()) {
-                            //todo bugtesta att kontroll av belopp fungerar
-
-                            //if(allAccounts.get(transfer.getFromAccountNumber()).directTransfer()) {}
-                            //transfer.setStatus(Transfer.TransferStatus.COMPLETED);
-                            //todo se över när directTransfer() är färdig.
-                            // Ta bort eventuella kontroller som redan sker i directTransfer.
-                            // Använd return-value i directtransfer
-                        } else {
-                            transfer.setStatus(Transfer.TransferStatus.FAILED);
-                        }
-                    } else {
-                        transfer.setStatus(Transfer.TransferStatus.FAILED);
-                    }
-                }
-            } else {
-                //Eftersom listan är sorterad så avbryts loopen när man kommit till bankuppdrag som ska genomföras imorgon eller senare.
-                break;
-            }
-        }
-
-        //Sparar listorna igen
-        FileService.INSTANCE.saveAccounts(allAccounts);
-        FileService.INSTANCE.saveTransfers(allTransfers);
-
-        //FileService.INSTANCE.saveCustomers(allCustomers); //todo ta bort (temporärt)
-
-        tempSortAndPrint("Alla borde vara markerade som FAILED pga felaktigt kontonr"); //todo ta bort alla tempSortAndPrint()
-
-
-    }
-
-    /**
-     * Raderar gamla transaktioner ifrån historiken
-     *
-     * @param monthsBeforeDeletingHistory antal månader som ska passera efter transaktionsdatum innan transaktionen tas bort ifrån historiken.
-     */
-    private static void deleteHistory(long monthsBeforeDeletingHistory) {
+        //Raderar gammal historik
         boolean keepDeleting = true;
-        //
         if (allTransfers.size() > 0) {
             while (keepDeleting) {
                 if (allTransfers.get(0).getTransferDate().plusMonths(monthsBeforeDeletingHistory).isBefore(LocalDate.now())) {
@@ -131,22 +44,37 @@ public class backgroundApp {
                 }
             }
         }
-    }
 
-    /**
-     * Temporär metod för att skriva ut allTransfers. Används bara för att kolla steg för steg att backgroundApp fungerar som den ska
-     *
-     * @param message meddelande före utskrift av allTransfers (till exempel "tog nyss bort alla transfers med belopp större än 500").
-     */
-    private static void tempSortAndPrint(String message) {
-        System.out.println(message);
-        Collections.sort(allTransfers, (a, b) -> a.getTransferDate().compareTo(b.getTransferDate()));
-        allTransfers.forEach(transfer -> {
-            System.out.println("FromAcc: " + transfer.getFromAccountNumber()
-                    + " ToAcc: " + transfer.getToAccountNumber()
-                    + " Amount: " + transfer.getAmount()
-                    + " Transferdate: " + transfer.getTransferDate()
-                    + " Status: " + transfer.getStatus());
-        });
+        //Hanterar resterande bankuppdrag
+        for (Transfer transfer : allTransfers) {
+            //Om transferDate inträffar före imorgon (idag eller tidigare)
+            if (transfer.getTransferDate().isBefore(LocalDate.now().plusDays(1))) {
+                //Hanterar status pending och struntar i cancelled, failed, complete
+                if (transfer.getStatus() == Transfer.TransferStatus.PENDING) {
+                    //Om avsändaren och mottagarens konton existerar
+                    if (allAccounts.containsKey(transfer.getFromAccountNumber()) && allAccounts.containsKey(transfer.getToAccountNumber())) {
+                        //Om det finns täckning för betalningen på kontot
+                        if (transfer.getAmount() <= allAccounts.get(transfer.getFromAccountNumber()).getBalance()) {
+                            //todo Kolla vilka kontroller som verkligen behöver göras och vilka som redan görs i directTransfer()
+                            if (allAccounts.get(transfer.getFromAccountNumber()).directTransfer(allAccounts.get(transfer.getToAccountNumber()), transfer.getAmount())) {
+                                transfer.setStatus(Transfer.TransferStatus.COMPLETED);
+                            } else {
+                                transfer.setStatus(Transfer.TransferStatus.FAILED);
+                            }
+                        } else {
+                            transfer.setStatus(Transfer.TransferStatus.FAILED);
+                        }
+                    } else {
+                        transfer.setStatus(Transfer.TransferStatus.FAILED);
+                    }
+                }
+            } else {
+                //Eftersom listan är sorterad så avbryts loopen när man kommit till bankuppdrag som ska genomföras imorgon eller längre fram.
+                break;
+            }
+        }
+
+        FileService.INSTANCE.saveAccounts(allAccounts);
+        FileService.INSTANCE.saveTransfers(allTransfers);
     }
 }
